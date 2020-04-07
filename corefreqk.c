f6ba67a06676e3f83dfc4eeefb8b09683c39fc87
CyrIng <labs@cyring.fr>
2020-04-07 20:06:02 +0200

    LookupProcessor() fix: Return as soon as the brand string is found (#172)

--- corefreqk.c
+++ corefreqk.c
@@ -1690,22 +1690,22 @@ void OverrideUnlockCapability(PROCESSOR_SPECIFIC *pSpecific)
 
 PROCESSOR_SPECIFIC *LookupProcessor(void)
 {
-	char *lookIn = NULL;
-
 	PROCESSOR_SPECIFIC *pSpecific;
     for (pSpecific = Arch[Proc->ArchID].Specific;
-	(pSpecific != NULL) && (pSpecific->Brand != NULL) && (lookIn == NULL);
-		pSpecific++)
+		(pSpecific != NULL) && (pSpecific->Brand != NULL);
+			pSpecific++)
     {
 	char **brands, *brand;
 	for (brands = pSpecific->Brand, brand = *brands;
-		(brand != NULL) && (lookIn == NULL);
+		brand != NULL;
 			brands++, brand = *brands)
 	{
-		lookIn = strstr(Proc->Features.Info.Brand, brand);
+		if (strstr(Proc->Features.Info.Brand, brand)) {
+			return (pSpecific);
+		}
 	}
     }
-	return (pSpecific);
+	return (NULL);
 }
 
 int Intel_MaxBusRatio(PLATFORM_ID *PfID)
