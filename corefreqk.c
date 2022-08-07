4be059220eb7aac582e8757da0e0855617546137
CyrIng <labs@cyring.fr>
2022-08-07 09:08:49 +0000

    [AMD_DataFabric_Cezanne] Removed the Experimental mode

--- corefreqk.c
+++ corefreqk.c
@@ -5919,11 +5919,7 @@ static PCI_CALLBACK AMD_DataFabric_Vermeer(struct pci_dev *pdev)
 
 static PCI_CALLBACK AMD_DataFabric_Cezanne(struct pci_dev *pdev)
 {
-    if (PUBLIC(RO(Proc))->Registration.Experimental) {
 	return AMD_17h_DataFabric(pdev, 1, (const unsigned short[]){0x18});
-    } else {
-	return (PCI_CALLBACK) -EAGAIN;
-    }
 }
 
 static void CoreFreqK_ResetChip(struct pci_dev *dev)
