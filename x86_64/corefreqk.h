4d9ede9f46718256f473feccdd7a5e04190c4cba
CyrIng <labs@cyring.fr>
2023-09-23 11:59:25 +0200

    [AMD][STP] Adding Ryzen Threadripper PRO 7975WX

--- x86_64/corefreqk.h
+++ x86_64/corefreqk.h
@@ -7981,6 +7981,19 @@ static PROCESSOR_SPECIFIC AMD_Zen4_STP_Specific[] = {
 	.Latch=LATCH_TGT_RATIO_UNLOCK|LATCH_CLK_RATIO_UNLOCK|LATCH_TURBO_UNLOCK\
 		|LATCH_HSMP_CAPABLE
 	},
+	{
+	.Brand = ZLIST("AMD Ryzen Threadripper PRO 7975WX"),
+	.Boost = {+8, 0},
+	.Param.Offset = {0, 0, 0},
+	.CodeNameIdx = CN_STORM_PEAK,
+	.TgtRatioUnlocked = 1,
+	.ClkRatioUnlocked = 0b10,
+	.TurboUnlocked = 0,
+	.UncoreUnlocked = 0,
+	.HSMP_Capable = 1,
+	.Latch=LATCH_TGT_RATIO_UNLOCK|LATCH_CLK_RATIO_UNLOCK|LATCH_TURBO_UNLOCK\
+		|LATCH_HSMP_CAPABLE
+	},
 	{0}
 };
 
